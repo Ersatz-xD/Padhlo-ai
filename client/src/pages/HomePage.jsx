ab919d4454899240736f2a758c9c991ce5afa581
Ayaan Ahmed <aayan.shazim@gmail.com>
2025-04-22 22:49:52 +0500

    Basic Home Page Done

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,5 +1,51 @@
+import React from "react";
+import "./homepage.css";
+import arrowIcon from "../assets/arrow-circle-right.png";
+import studyGroup from "../assets/35020245_8262271.jpg";
+
 const HomePage = () => {
-    return <h1>Home Page</h1>
-  }
-  export default HomePage
-  
+  return (
+    <div className="main-heading">
+      <h1 className="text">AI-Powered Study Assisstant</h1>
+      <h4 className="text">Learn Better. Faster. Easier.</h4>
+
+      <div className="go-to-about">
+        <span className="about-btn">About Us</span>
+        <button className="arrow-btn">
+        <img src={arrowIcon} alt="arrow icon" />
+        </button>
+      </div>
+
+      <div className="lower-part">
+        <div className="study-image">
+        <img
+          className="study-group"
+          src={studyGroup}
+          alt="vector-study-group"
+        />
+        </div>
+        
+        <div className="right-lower">
+          <div className="go-to-notes">
+            <h4 className="light-text">Notes Helper</h4>
+            <p className="lighter-text">Your Messy Notes - Made Meaningful</p>
+            <div className="go-to-div">
+            <button className="go-to-btn-notes">Open it</button>
+            </div>
+            
+          </div>
+
+          <div className="go-to-quiz">
+            <h4 className="light-text">Quiz Generator</h4>
+            <p className="lighter-text">From Concepts to Questions — Instantly</p>
+            <div className="go-to-div">
+            <button className="go-to-btn-quiz">Open it</button>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  );
+};
+export default HomePage;
