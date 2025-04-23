1b2feacf62eb427fce7ecc67deb8a0390c7ea1e4
Ayaan Ahmed <aayan.shazim@gmail.com>
2025-04-23 17:16:52 +0500

    Added heading and components

--- client/src/pages/NotesPage.jsx
+++ client/src/pages/NotesPage.jsx
@@ -1,5 +1,27 @@
+import React from "react";
+import "./notespage.css";
+import NotesGeneratorBox from "../modules/notes-helper/components/NotesGeneratorBox";
+import GeneratedNotes from "../modules/notes-helper/components/GeneratedNotes";
+
 const NotesPage = () => {
-    return <h1>Notes Assisstant</h1>
+    return (
+      <div className="parent-div">
+
+      <div className="main-heading">
+      <h1 className="text">Notes Helper</h1>
+      <h6 className="text slogan">Turn Rough Notes into Perfect Study Material</h6>
+      </div>
+
+      <div className="notes-generator-box">
+      <NotesGeneratorBox />
+      </div>
+
+      <div className="generated-notes-box">
+        <GeneratedNotes />
+      </div>
+
+      </div>
+    )
   }
   export default NotesPage
   
