fd495552d12d62ade0a43da7a059ed9564a0d89a
Ayaan Ahmed <aayan.shazim@gmail.com>
2025-04-23 19:16:02 +0500

    add temp content

--- client/src/pages/QuizPage.jsx
+++ client/src/pages/QuizPage.jsx
@@ -1,5 +1,19 @@
+import React from "react";
+import "./quizpage.css";
+
 const QuizPage = () => {
-    return <h1>Quiz Generator</h1>
-  }
-  export default QuizPage
-  
+  return (
+    <div className="main-parent">
+      <div class="under-dev-container">
+        <h1 class="under-dev-heading">🚧 This Page is Under Development</h1>
+        <p class="under-dev-text">
+          We're working hard to bring this feature to life. Check back soon!
+        </p>
+        <a href="/" class="under-dev-btn">
+          Go Back Home
+        </a>
+      </div>
+    </div>
+  );
+};
+export default QuizPage;
