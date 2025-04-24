6eb2ba120aa45410f5e34e46a677da6b5df42db6
Ayaan Ahmed <aayan.shazim@gmail.com>
2025-04-24 23:18:27 +0500

    functionality to copy

--- client/src/modules/notes-helper/components/GeneratedNotes.jsx
+++ client/src/modules/notes-helper/components/GeneratedNotes.jsx
@@ -1,8 +1,23 @@
-import React from "react";
+import React, {useRef} from "react";
 import ReactMarkdown from 'react-markdown';
+import { marked } from "marked";
 import "./generatednotes.css";
 
 const GeneratedNotes = ( {explanation} ) => {
+  const textRef = useRef(null);
+
+  const handleCopy = async () => {
+    try {
+      const html = marked.parse(explanation); // convert markdown to HTML
+      const blob = new Blob([html], { type: "text/html" });
+      await navigator.clipboard.write([new ClipboardItem({ "text/html": blob })]);
+      alert("Copied with formatting!");
+    } catch (err) {
+      console.error("Copy failed:", err);
+    }
+  };
+
+
   return (
     <div className="main-parent">
       <div className="generated-notes">
@@ -10,12 +25,12 @@ const GeneratedNotes = ( {explanation} ) => {
         <div className="notes-box">
           {/* <p className="notes-heading">{topic}</p> */}
           {/* <p className="notes-exp"> {explanation} </p> */}
-          <p className="notes-exp">
+          <div className="notes-exp" ref={textRef}>
           <ReactMarkdown >{explanation}</ReactMarkdown>
-          </p>
+          </div>
           
         </div>
-        <button id="btn" >Copy To Clipboard</button>
+        <button className="copy-btn" onClick={handleCopy} >Copy To Clipboard</button>
       </div>
     </div>
   );
