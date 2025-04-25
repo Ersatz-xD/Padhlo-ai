1a1cf4e2a43fc3712e10ef689b61fd27ae94d2e3
Ayaan Ahmed <aayan.shazim@gmail.com>
2025-04-25 22:37:02 +0500

    connect database in server

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,13 @@
 import app from "./app.js";
 import dotenv from 'dotenv';
+import connectDB from './src/config/db.js';
 
 dotenv.config();
 
 const PORT = process.env.PORT;
+
+//conect DB
+connectDB();
 
 app.listen(
     PORT, () => {
