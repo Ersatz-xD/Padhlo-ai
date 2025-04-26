9d3386960fcf2e1632630f1d05ab4fef09c6c14c
Ayaan Ahmed <aayan.shazim@gmail.com>
2025-04-27 02:11:44 +0500

    get and send userID

--- client/src/api/notesHelper.js
+++ client/src/api/notesHelper.js
@@ -1,18 +1,30 @@
 import axios from 'axios';
 
 const getNotesFromAI = async (topic, roughNotes) => {
-    try {
-        const response = await axios.post('http://localhost:5000/api/generate-summary', {
-      topic,
-      notes: roughNotes,
-    });
-    return response.data.summary;
+  try {
+    const token = localStorage.getItem('token');
+    const userId = localStorage.getItem("userId");
 
+    const response = await axios.post('http://localhost:5000/api/generate-summary', 
+      {
+        topic,
+        notes: roughNotes,
+        userId
+      }, 
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    
 
-    } catch (error) {
-    console.error("Error fetching notes: ", error);
-    return "Failed to generate notes. Please try again!"    
-    }
+      return response.data.summary;
+
+  } catch (error) {
+      console.error("Error fetching notes: ", error);
+      return "Failed to generate notes. Please try again!";
+  }
 };
 
 export {getNotesFromAI} 
