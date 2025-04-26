61ccd1e70ef3facbd85de1097486f76aa2bc8b9e
Ayaan Ahmed <aayan.shazim@gmail.com>
2025-04-27 02:13:46 +0500

    Update notesRoutes.js

--- server/src/routes/notesRoutes.js
+++ server/src/routes/notesRoutes.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { generateSummary } from '../controllers/notesController.js';
-
+import { generateSummary, getAllNotes } from '../controllers/notesController.js';
+import auth from '../middlewares/auth.js';
 const router = express.Router();
 
-router.post('/generate-summary', generateSummary);
+router.post("/generate-summary", generateSummary);  
+router.get("/get-all-notes", auth, getAllNotes);   
+
 
 export default router;
