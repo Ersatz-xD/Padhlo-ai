cf43c8a5007af6cf87e79a49f94d1e4bf086e9c0
Ayaan Ahmed <aayan.shazim@gmail.com>
2025-04-26 14:01:51 +0500

    fn to login

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,3 +1,4 @@
+import Joi from "joi";
 import { User, validate } from "../models/User.js";
 import bcrypt from "bcrypt";
 
@@ -25,4 +26,43 @@ const registerUser = async (req, res) => {
   }
 };
 
-export  {registerUser}
+//validation
+const loginValidation = (data) => {
+    const schema = Joi.object({
+      email: Joi.string().email().required().label("Email"),
+      password: Joi.string().required().label("Password"),
+    });
+    return schema.validate(data);
+  };
+
+
+//Login
+const loginUser = async (req, res) => {
+  try {
+    const { error } = loginValidation(req.body);
+    if (error) {
+      return res.status(400).send({ message: error.details[0].message });
+    }
+
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(401).send({ message: "Invalid email or password!" });
+    }
+
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!validPassword) {
+      return res.status(401).send({ message: "Invalid email or password!" });
+    }
+
+    const token = User.generateAuthToken();
+    res.status(200).send({ data: token, message: "Logged in successfully!" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ message: "Internal Server Error." });
+  }
+};
+
+export { registerUser, loginUser };
