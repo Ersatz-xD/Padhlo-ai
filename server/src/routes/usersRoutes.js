4935154f7ee79a84542269f77a7d37b5724649b6
Ayaan Ahmed <aayan.shazim@gmail.com>
2025-04-26 14:01:41 +0500

    added login route

--- server/src/routes/usersRoutes.js
+++ server/src/routes/usersRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { registerUser } from "../controllers/userController.js";
+import { registerUser, loginUser } from "../controllers/userController.js";
 
 const router = express.Router();
 
 // Route for registration
 router.post("/register", registerUser);
+// Login route
+router.post("/login", loginUser);
 
 export default router;
